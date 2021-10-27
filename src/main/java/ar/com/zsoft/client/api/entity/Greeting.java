8755fb8d9c5af6bcfc86d5a02fee714f19be3b97
Cristian R Zerpa <cristianzerpa89@gmail.com>
2021-10-26 21:19:09 -0300

    added greeting files

---
+++ src/main/java/ar/com/zsoft/client/api/entity/Greeting.java
@@ -0,0 +1,20 @@
+package ar.com.zsoft.client.api.entity;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
